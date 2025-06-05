d205df10ddfc2063e7e3ee27d8c6fd61bbcd971c
IGIRIMPUHWE Dositha <igirimpuhwedosta@gmail.com>
2025-06-05 20:02:54 +0300

    add footer

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-[#0B3B2E] text-white text-3xl'>
+     TegaBus
+    </div>
+  )
+}
+
+export default Footer   
