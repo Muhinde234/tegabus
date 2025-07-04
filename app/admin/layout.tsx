4082d94cc076f43dc6b7193217b302483e1a697a
IGIRIMPUHWE Dositha <igirimpuhwedosta@gmail.com>
2025-07-04 09:50:56 +0300

    add responsive to public page

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,13 @@
 
-import { ReactNode } from 'react';
 import Sidebar from '@/components/dashboard/sidebar';
+import { ReactNode } from 'react';
+
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar/>
+      
       <main className="flex-1 ml-64  bg-gray-50 min-h-screen">
         {children}
       </main>
