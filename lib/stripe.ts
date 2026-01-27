e2e01a84a97e635cac51f6701509e28474b9c5b1
IGIRIMPUHWE Dositha <igirimpuhwedosta@gmail.com>
2026-01-27 09:38:35 +0100

    updated some dependencies

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,7 +3,7 @@ import "server-only";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  // https://github.com/stripe/stripe-node#configuration
+  
   apiVersion: "2025-08-27.basil", // ✅ updated API version
   appInfo: {
     name: "nextjs-with-stripe-typescript-demo",
