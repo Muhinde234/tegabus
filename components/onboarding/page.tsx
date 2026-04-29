3af9b623694f6a8de3605b8954d2afbd7a9cf417
Muhinde234 <igirimpuhwedosta@gmail.com>
2026-04-29 20:01:30 +0200

    add space for the company

--- components/onboarding/page.tsx
+++
@@ -1,4 +0,0 @@
-// This file is intentionally empty.
-// The onboarding page is at: app/[locale]/(guest)/onboarding/page.tsx
-// The form component is at: components/onboarding/CompanyForm.tsx
-export {};
